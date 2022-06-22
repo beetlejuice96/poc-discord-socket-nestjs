52395ee16c2be2fe6074c5c74f9aec686ac51ed9
beetlejuice96 <dilanbernbae043@gmail.com>
2022-06-22 10:02:09 -0300

    investigacion

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,6 +1,12 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectDiscordClient, Once } from '@discord-nestjs/core';
-import { Client } from 'discord.js';
+import {
+  Client,
+  FetchGuildOptions,
+  Guild,
+  GuildListMembersOptions,
+  GuildResolvable,
+} from 'discord.js';
 
 @Injectable()
 export class BotService {
@@ -12,13 +18,33 @@ export class BotService {
   ) {}
 
   @Once('ready')
-  onReady() {
+  async onReady() {
     this.logger.log(`Bot ${this.client.user.tag} was started!`);
-    let guilds = this.client.guilds;
-    guilds.fetch({
+    // let guild: Guild;
+    // guild.id = '916416894666420247';
+    const guilds = this.client.guilds;
+    // const allGuilds = guilds.fetch({ guild });
+    const guildfinded: GuildResolvable = guilds.cache.find(
+      (guild) => guild.id === '916416894666420247',
+    );
+    const guildFindedWithUsers = await guilds.fetch({
+      guildfinded,
+      withCounts: true,
+    } as unknown as FetchGuildOptions);
+    console.log(guildfinded);
 
-    })
+    const membersManager = guildfinded.members;
+    try {
+      const membersActives = await membersManager.list({
+        limit: 100,
+        count: true,
+      } as GuildListMembersOptions);
+      console.log('members activos:', membersActives);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // const users = this.client.users;
+    // console.log(users.cache);
   }
-
-
 }
